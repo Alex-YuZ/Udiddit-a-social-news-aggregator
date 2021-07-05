feccfb881177b850f30c9ed5206c7ffe9449a301
Alex-Yu <alexyuzhu@gmail.com>
2021-07-05 10:27:30 +0800

    feat: Create 'posts' table based on guidlines

--- udiddit_social_news_aggregator.sql
+++ udiddit_social_news_aggregator.sql
@@ -23,3 +23,29 @@ CREATE TABLE "topics" (
   CONSTRAINT "non_empty_topic_name" CHECK(LENGTH(TRIM("topic_name"))>0)
  );
 CREATE INDEX "topic_name_index" ON "topics" ("topic_name");
+
+-- Guidline #1: c.
+CREATE TABLE "posts" (
+  "id" SERIAL PRIMARY KEY,
+  "title" VARCHAR(100) NOT NULL,
+  "url" VARCHAR,
+  "content" TEXT,
+  "topic_id" INTEGER NOT NULL,
+  "user_id" INTEGER,
+  "created_at" TIMESTAMP WITH TIME ZONE,
+     
+  CONSTRAINT "non_empty_title" CHECK(LENGTH(TRIM("title"))>0),
+     
+  CONSTRAINT "url_or_text_check"
+    CHECK ((LENGTH(TRIM("url"))=0 AND LENGTH(TRIM("content"))!=0) OR
+           (LENGTH(TRIM("url"))!=0 AND LENGTH(TRIM("content"))=0)),
+     
+  CONSTRAINT "fkey_topic_id"
+    FOREIGN KEY ("topic_id") REFERENCES "topics" ON DELETE CASCADE,
+     
+  CONSTRAINT "fkey_user_id"
+    FOREIGN KEY ("user_id") REFERENCES "users" ON DELETE SET NULL
+);
+CREATE INDEX "latest_posts_given_topic" ON "posts" ("topic_id", "created_at");
+CREATE INDEX "latest_posts_given_user" ON "posts" ("user_id", "created_at");
+CREATE INDEX "url_moderation" ON "posts" ("url");
